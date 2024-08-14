5c5025336c11dde10372a2544c03f4d923f6e272
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-08-15 00:47:24 +0530

    navigation bar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Chinthaka", lastName: "Dinadasa" };
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+      {children}
     </main>
   );
 }
