ff8ae74082454f40286907cdb65d06c99f7d1fa1
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-08-16 12:37:41 +0530

    bank integration done

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -12,6 +12,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "../plaid/PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter()
@@ -37,7 +38,19 @@ const AuthForm = ({ type }: { type: string }) => {
     setIsLoading(true);
     try {
       if (type === 'sign-up') {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+        const newUser = await signUp(userData);
         setUser(newUser)
       }
       if (type === 'sign-in') {
@@ -82,7 +95,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* Plaidlink */}</div>
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary"/>
+        </div>
       ) : (
         <>
           <Form {...form}>
